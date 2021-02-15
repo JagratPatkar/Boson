85ef624cc639c8ba588ca288af9c15980016482d
JagratPatkar <jagratpatkar@gmail.com>
2021-02-15 12:34:24 +0530

    Update

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -3,31 +3,24 @@ using namespace std;
 int Lexer::getToken(){
     char curChar = ' ';
     
-    while(isspace(curChar) && !source.eof())
-        source.get(curChar);
-
+    while(isspace(curChar) && !source.eof())source.get(curChar);
     if(isdigit(curChar)){
         NumberString = "";
         NumberString += curChar;
         return extractNumber();
     }
-
     if(isalpha(curChar)){
         Identifier = "";
         Identifier += curChar;
         return extractIdentifier();
     }
-
     auto it = SymbolRegistry.find(curChar);
     if(it != SymbolRegistry.end()) return it->second;
     int tmp;
-
     if(curChar == '=') return (tmp = peekOneAhead('=',token_equal_to)) ? tmp : token_assignment_op;
     if(curChar == '<') return (tmp = peekOneAhead('=',token_less_than_eq)) ? tmp : token_less_then;
     if(curChar == '>') return (tmp = peekOneAhead('=',token_greater_than_eq)) ? tmp : token_greater_then;  
     if(curChar == '!'){ if((tmp = peekOneAhead('=',token_less_than_eq))) return tmp; }    
-
-
     if(source.eof()) return token_eof;
     printf("Unknown token \n");
     exit(-1);
