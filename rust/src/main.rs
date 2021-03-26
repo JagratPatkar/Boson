10952eddea4b34081027d3e2b8b5bda4791c8577
JagratPatkar <jagratpatkar@gmail.com>
2021-03-26 21:42:55 +0530

    Update

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,10 @@
 use structopt::StructOpt; 
+use anyhow::{Context,Result};
+use utf8_chars::{BufReadCharsExt};
+use utf8_chars::Chars;
+use std::fs::File;
+use std::io::BufReader;
+use std::path::PathBuf;
 
 #[derive(StructOpt)]
 struct Cli{
@@ -6,17 +12,55 @@ struct Cli{
     path : std::path::PathBuf,
 }
 
+#[allow(dead_code)]
 enum Token{
     LeftParen(char),
     RightParen(char),
     Op(char),
     IntNum(i64),
     DoubleNum(f64),
+    String(String)
+}
+
+#[allow(dead_code)]
+struct Lexer{
+    reader :BufReader<File>,
+    token : char
+}
+
+impl Lexer 
+{
+    fn new(name : PathBuf) -> Result<Lexer> {
+        let f = File::open(name).with_context(|| format!("Failed to read file"))?;
+        Ok(Lexer {
+            reader : BufReader::new(f),
+            token : ' '
+        })
+    }  
+
+   
+    // fn getNextToken(&self) -> Option<Token> {
+    //     let mut char = self.reader.chars().map(|x| x.unwrap()).peekable();
+    //     let char = char.next().unwrap();
+
+    // }
+
+    //Will be used later for testing
+    // fn testNew(name : String) -> Lexer<T> {
+
+    // }
+
 
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
-    println!("{}",content);
+    let content = std::fs::read_to_string(&args.path).with_context(|| format!("could not read file '{:?}'",args.path))?;
+    let mut f = File::open(&args.path)?;
+    let mut reader = BufReader::new(f);
+    let mut char = reader.chars().map(|x| x.unwrap()).peekable();
+    println!("{}",char.next().unwrap());
+    // let lexer  = Lexer::new(args.path)?;
+    
+    return Ok(());
 }
