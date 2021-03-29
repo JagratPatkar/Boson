f974a7602980df51d1a8822659d7de387e5767b9
JagratPatkar <jagratpatkar@gmail.com>
2021-03-29 20:09:32 +0530

    Update

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -35,9 +35,11 @@ impl Keyword {
 
 #[derive(Debug)]
 enum Token{
-    Keyword(Keyword),
-    Identifier(String),
-    EOF
+    KEYWORD(Keyword),
+    IDENTIFIER(String),
+    INT(i32),
+    EOF,
+    ERROR
 }
 
 #[allow(dead_code)]
@@ -62,11 +64,11 @@ impl Lexer
         'outer:loop{
             match token {
                 Some(t) if t.is_ascii_whitespace() => { token = char.next(); },
-                Some(t) if t.is_alphabetic() => {
+                Some(t) if t.is_ascii_alphabetic() => {
                     let mut ch :char = t;
                     let mut iden = String::from("");
                     iden.push(t);
-                    while ch.is_alphanumeric(){
+                    while ch.is_ascii_alphanumeric(){
                         token = char.next();
                         if let Some(it) = token { iden.push(it); }
                         else { continue 'outer; }
@@ -74,10 +76,23 @@ impl Lexer
                         else { break; }
                     }
                     if let Some(keyword) = Keyword::is_keyword(&iden) 
-                    { self.token = Some(Token::Keyword(keyword)); break; }
-                    else { self.token = Some(Token::Identifier(iden)); break; }
+                    { self.token = Some(Token::KEYWORD(keyword)); break; }
+                    else { self.token = Some(Token::IDENTIFIER(iden)); break; }
                 },
-                // Some(t) if t.
+                Some(t) if t.is_digit(10) => {
+                    let mut ch : char = t;
+                    let mut num : String = String::new();
+                    num.push(ch);
+                    while ch.is_digit(10) {
+                        token = char.next();
+                        if let Some(it) = token { num.push(it); }
+                        else { continue 'outer; }
+                        if let Some(c) = char.peek() { ch = c.clone(); }
+                        else { break; }
+                    }
+                    if let Ok(number) = num.parse::<i32>(){ self.token = Some(Token::INT(number)); break; }
+                    else { self.token = Some(Token::ERROR); break; }
+                },
                 Some(_) => {},
                 None => { self.token = Some(Token::EOF); }
             }
@@ -95,6 +110,12 @@ impl Lexer
 fn main() -> Result<()> {
     let args = Cli::from_args();
     let mut lexer = Lexer::new(args.path)?;
+    lexer.get_next_token();
+    lexer.print_token();
+    lexer.get_next_token();
+    lexer.print_token();
+    lexer.get_next_token();
+    lexer.print_token();
     lexer.get_next_token();
     lexer.print_token();
     lexer.get_next_token();
